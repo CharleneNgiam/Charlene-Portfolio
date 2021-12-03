9078fd75084cf80998aeebb61e103a883fd9c33a
CharleneNgiam <s10227262@connect.np.edu.sg>
2021-12-03 21:03:25 +0800

    added javascript for submit button

---
+++ script.js
@@ -0,0 +1,8 @@
+// contact me, after user click submits, popup message will thank them
+function myFunction() {
+    alert("Thankyou for reaching out, i will get back to you as soon as possible. Thankyou for the message once again!");
+  }
+
+
+
+
